f8d7e9ab4cf78338234c1a087ecdd78b61e4f9f0
janislehmann <mail@lhmn.me>
2024-11-13 19:57:54 +0100

    remove fourth node

--- teraform/locals.tf
+++ teraform/locals.tf
@@ -5,8 +5,7 @@ locals {
     k3s_master = [
         {name = "devastator-0", vmid = 200, ip = "192.168.0.20", target_node = "homelab-0"},
         {name = "devastator-1", vmid = 201, ip = "192.168.0.21", target_node = "homelab-0"},
-        {name = "devastator-2", vmid = 202, ip = "192.168.0.22", target_node = "homelab-1"},
-        {name = "devastator-3", vmid = 203, ip = "192.168.0.23", target_node = "homelab-1"}
+        {name = "devastator-2", vmid = 202, ip = "192.168.0.22", target_node = "homelab-1"}
     ]
 
     k3s_agent = []  
